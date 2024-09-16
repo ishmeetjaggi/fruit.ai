da3fb421d6dfd1bd83468a16a167dc9248fdc843
ishmeetjaggi <93987284+ishmeetjaggi@users.noreply.github.com>
2024-09-16 15:15:43 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -3,9 +3,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = 'mysql+pymysql://root:12345678@localhost:3306/fruits_users'
+# Update with your Aiven MySQL credentials
+URL_DATABASE = 'mysql+pymysql://avnadmin:AVNS_hpdcV-HWz6FMuagZXP3@mysql-134a7b73-ishmeetkaurj-61b3.c.aivencloud.com:25099/defaultdb'
+
+# Create the SQLAlchemy engine
 engine = create_engine(URL_DATABASE)
 
-LocalSession=sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Create a configured "Session" class
+LocalSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base=declarative_base()
+# Create a Session
+session = LocalSession()
+
+# Declare a Base
+Base = declarative_base()
